75f270d2aae48e952fb062d781a651c22fa1aee8
andre-barrera <andre.barrera.99@gmail.com>
2025-05-11 16:11:37 -0600

    updated changes

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumberNum = randomGenerator.Next(1,100);
+        
+        int userGuessNum = -1;
+        int numberOfGuesses = 0;
+
+        while (magicNumberNum != userGuessNum)
+        {
+        Console.Write($"What is your guess? ");
+        string userGuess = Console.ReadLine();
+        userGuessNum = int.Parse(userGuess);
+        numberOfGuesses += 1;
+
+        if (userGuessNum == magicNumberNum)
+        {
+            Console.WriteLine("Your guessed it!");
+            Console.WriteLine($"You had {numberOfGuesses} attempts");
+        }
+        else if (userGuessNum > magicNumberNum)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (userGuessNum < magicNumberNum)
+        {
+            Console.WriteLine("Higher");
+        }
+        }
+        
     }
 }
