bc12a371f68fa9af99cd2780a5c82350e32608dc
andre-barrera <andre.barrera.99@gmail.com>
2025-05-10 21:28:46 -0600

    updated changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,12 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-        string fist_name = Console.ReadLine();
+        string fistName = Console.ReadLine();
 
         Console.WriteLine("What is your last name?");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
         
-        Console.WriteLine($"Your name is {last_name}, {fist_name} {last_name}");
+        Console.WriteLine($"Your name is {lastName}, {fistName} {lastName}");
     }
 }
 
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+        string sign = "";
+        Console.WriteLine("Please enter your grade percentage:");
+        string gradePercentage = Console.ReadLine();
+        int gradePercentageNum = int.Parse(gradePercentage);
+
+        if (gradePercentageNum >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentageNum >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentageNum >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentageNum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        int calculateSign = gradePercentageNum % 10;
+        if (letter == "A" && gradePercentageNum < 93)
+        {
+            sign = "-";
+        }
+        else if (letter != "A" && letter != "F")
+        {
+            if (calculateSign >= 7)
+            {
+                sign = "+";
+            }
+            else if (calculateSign < 3)
+            {
+                sign ="-";
+            }
+        }
+
+        Console.WriteLine ($"Your grade is {letter}{sign}");
     }
 }
