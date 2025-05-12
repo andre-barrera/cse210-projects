6d99419bd0c88702c966219fa716391a067910a7
andre-barrera <andre.barrera.99@gmail.com>
2025-05-11 21:30:44 -0600

    updated changes

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List <int> numbers = new List<int>();
+        int newNumberInt = -1;
+        int numSum = 0;
+        int largestNumber = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        while (newNumberInt != 0)
+        {
+        Console.Write("Enter number: ");
+        string newNumber = Console.ReadLine();
+        newNumberInt = int.Parse(newNumber);
+
+        if (newNumberInt != 0)
+        {
+        numbers.Add(newNumberInt);
+        numSum += newNumberInt;
+
+        if(newNumberInt > largestNumber)
+        {
+            largestNumber = newNumberInt;
+        }
+
+        }
+        }
+
+        int numOfEntries = numbers.Count;
+        int aveNum = numSum / numOfEntries;
+
+
+
+        Console.WriteLine($"The sum is {numSum}");
+        Console.WriteLine($"The average is {aveNum}");
+        Console.WriteLine($"The largest number is {largestNumber}");
+
+
+
+
+
+
     }
 }
